d2eb5923a349d72f1852b0a589e540cbd922b256
Fauthe <manishlama731@gmail.com>
2022-12-15 07:13:00 +0545

    Dec 15 Classwork Completed

--- Classworks/DirectoryAndFiles.cs
+++ Classworks/DirectoryAndFiles.cs
@@ -1,16 +1,25 @@
 class DirectoryAndFiles
 {
-    string folderPath = @"G:\gitCloning";
 
 
     public void CreateDirectoriesAndFiles()
     {
-        string rootFolderPath = folderPath + "\\RootFolder";
-        Directory.CreateDirectory(rootFolderPath);
+        string rootFolderPath = @"G:\gitCloning\RootFolder";
+        // for (int i = 1; i < 11; i++)
+        // {
+        //     string childFolderPath = rootFolderPath + "\\Folder" + i;
+        //     Directory.CreateDirectory(childFolderPath);
+
+
+        // }
+
         for (int i = 1; i < 11; i++)
         {
-            string childFolderPath = rootFolderPath + "\\Folder" + i;
-            Directory.CreateDirectory(childFolderPath);
+            string childFilePath = rootFolderPath + "\\Folder" + i;
+            Directory.CreateDirectory(childFilePath);
+            string fileName = $"{i}.txt";
+            string fullPath = @$"{childFilePath}\{fileName}";
+            File.WriteAllText(fullPath, $"I am in file {i}");
         }
 
     }
