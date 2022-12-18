f97db858ed873f349a8a218ba4e6f618e13df39c
Fauthe <manishlama731@gmail.com>
2022-12-18 07:49:40 +0545

    More On LINQ

--- Fundamentals/LINQ.cs
+++ Fundamentals/LINQ.cs
@@ -3,6 +3,7 @@
 class LINQ
 {
     int[] numbers = { 3, 4, 6, 8, 1, 3, 4, 51, 45, 67, 12, 34 };
+
     void LearnToQuery()
     {
         // List all even numbers from "numbers" array
@@ -59,5 +60,47 @@ class LINQ
          3. Convert all countries to uppercase.
          */
 
+        // List 5 items skipping first two
+        var fiveItems = numbers.Skip(2).Take(5);
+        PrintValues(fiveItems, "Skip-Take");
+
+        // Check if any item is even number
+        var isThereAnyEvenNumber = numbers.Any(x => x % 2 == 0);
+        Console.WriteLine($"Any Even: {isThereAnyEvenNumber}");
+
+        // Check if all items are even numbers
+        var isAllNumbersEven = numbers.All(x => x % 2 == 0);
+        Console.WriteLine($"All Even: {isAllNumbersEven}");
+
     }
+    public void LearnToQueryOnObjectCollection()
+    {
+        Person p1 = new();
+        p1.fullName = "Manish Kumar Tamang";
+        p1.gender = 'M';
+        p1.height = 170.23f;
+
+        Person p2 = new() { fullName = "Ram Basnet", gender = 'M', height = 160.34f };
+        Person p3 = new() { fullName = "Gita Basnet", gender = 'F', height = 164.34f };
+        Person p4 = new() { fullName = "Ganesh Joshi", gender = 'M', height = 161.34f };
+        Person p5 = new() { fullName = "Rita Basnet", gender = 'F', height = 180.38f };
+        List<Person> people = new List<Person>() { p1, p2, p3, p4, p5 };
+
+        // people.Add(p1);
+        // people.Add(p2);
+        // people.Add(p3);
+        // people.Add(p4);
+        // people.Add(p5);
+
+        // List All females
+        var females = people.Where(x => x.gender == 'F');
+
+        // List All Males is ascending order of their height
+        // Contextual keywords (from, where, orderby, select)
+        var males = from x in people
+                    where x.gender == 'M'
+                    orderby p1.height
+                    select x;
+    }
+
 }
