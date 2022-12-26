6dca3ae11ea19b61da0e90e24431373c08e4757f
Fauthe <manishlama731@gmail.com>
2022-12-26 07:47:53 +0545

    More About View and Controller

--- Web Development/HelloWeb/Controllers/NumberController.cs
+++ Web Development/HelloWeb/Controllers/NumberController.cs
@@ -2,8 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 
 public class NumberController : Controller
 {
-    public IActionResult Greet() //   /number/greet
+    public IActionResult MTable() //   /number/greet
     {
         return View();
+    }
+
+    public IActionResult MultiplicationTable(int n)
+    {
+        return View(n);
+    }
+
+    public IActionResult MultiplicationTableFromTo(int from, int to)
+    {
+
+        return View((from, to));
     }
 }
