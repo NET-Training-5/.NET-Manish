1dfb5560e89112dab99da41665f3b8ea7e6c10e4
Fauthe <manishlama731@gmail.com>
2022-12-27 07:29:11 +0545

    Data Passed From Controller To View.

--- Web Development/HelloWeb/Controllers/EmployeeController.cs
+++ Web Development/HelloWeb/Controllers/EmployeeController.cs
@@ -4,6 +4,86 @@ public class EmployeeController : Controller
 {
     public IActionResult EmployeeDetails()
     {
-        return View();
+        Employee e1 = new Employee()
+        {
+            Name = "Manish KumarTamang",
+            Sex = Gender.Male,
+            Email = "manishlama731@gmail.com",
+            Designation = "Database Administrator Trainee",
+            Department = "IT Department",
+            Phone = "9849451903",
+            Address = "Balkumari, Lalitpur"
+        };
+
+
+        Employee e2 = new Employee()
+        {
+            Name = "Neerajan Aryal",
+            Sex = Gender.Male,
+            Email = "neerajan3636@gmail.com",
+            Designation = "Database Administrator Intern",
+            Department = "IT Department",
+            Phone = "9812345678",
+            Address = "Nepaltar, Kathmandu"
+        };
+
+        Employee e3 = new Employee()
+        {
+            Name = "Kusum Khatri",
+            Sex = Gender.Female,
+            Email = "kusum123@gmail.com",
+            Designation = "Data Analyst Intern",
+            Department = "IT Department",
+            Phone = "9812345678",
+            Address = "Lubhu, Lalitpur"
+        };
+
+        Employee e4 = new Employee()
+        {
+            Name = "Sarfaroz Alam",
+            Sex = Gender.Male,
+            Email = "sarfaroz123@gmail.com",
+            Designation = "Accountant Intern",
+            Department = "Account & Finance",
+            Phone = "9823567867",
+            Address = "Balkumari, Lalitpur"
+        };
+
+        Employee e5 = new Employee()
+        {
+            Name = "Bipina K.C",
+            Sex = Gender.Female,
+            Email = "bipinia123@gmail.com",
+            Designation = "HR",
+            Department = "HR & Admin",
+            Phone = "9823456789",
+            Address = "Imadol, Lalitpur"
+        };
+
+        Employee e6 = new Employee()
+        {
+            Name = "Sarun Dangol",
+            Sex = Gender.Male,
+            Email = "sarun123@gmail.com",
+            Designation = "System Admin Trainee",
+            Department = "System Department",
+            Phone = "9812367897",
+            Address = "Sunakothi, Lalitpur"
+        };
+
+        Employee e7 = new Employee()
+        {
+            Name = "Saugat Nepal",
+            Sex = Gender.Male,
+            Email = "saugat123@gmail.com",
+            Designation = "System Intern",
+            Department = "System Department",
+            Phone = "9878675645",
+            Address = "Koteshor, Kathmandu"
+        };
+
+
+        List<Employee> employees = new List<Employee>() { e1, e2, e3, e4, e5, e6, e7 };
+        return View(employees);
     }
 }
---
+++ Web Development/HelloWeb/Models/Employee.cs
@@ -0,0 +1,10 @@
+public class Employee
+{
+    public string Name { get; set; }
+    public Gender Sex { get; set; }
+    public string Email { get; set; }
+    public string Designation { get; set; }
+    public string Department { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+}
