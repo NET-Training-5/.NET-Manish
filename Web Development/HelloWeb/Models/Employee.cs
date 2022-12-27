e9d65399303275fe984bcc5cf6f92c0da73af2c2
Fauthe <manishlama731@gmail.com>
2022-12-27 19:43:11 +0545

    Visual Studio Code Ended

--- Web Development/HelloWeb/Models/Employee.cs
+++ Web Development/HelloWeb/Models/Employee.cs
@@ -3,6 +3,7 @@ public class Employee
     public string Name { get; set; }
     public Gender Sex { get; set; }
     public string Email { get; set; }
+    public DateTime JoinDate { get; set; }
     public string Designation { get; set; }
     public string Department { get; set; }
     public string Phone { get; set; }
