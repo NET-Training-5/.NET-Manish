65d78eee81ef0e05eddd7875377a00d26a68dbdc
Fauthe <manishlama731@gmail.com>
2022-12-30 06:48:08 +0545

    Learned ADO .NET

---
+++ Web Development/HumanResources/HumanResources.Web/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using HumanResources.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace HumanResources.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			List<Employee> employees = new List<Employee>();
+			// Fetch Data From DB
+			//SqlConnection connection;
+			//SqlCommand command;
+			//SqlDataReader reader;
+			string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=HumanResources_Test;"
+			+ "Integrated Security=true";
+
+			// Provide the query string with a parameter placeholder.
+			string queryString =
+				"SELECT * from dbo.Employee";
+
+			// Create and open the connection in a using block. This
+			// ensures that all resources will be closed and disposed
+			// when the code exits.
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				// Create the Command and Parameter objects.
+				SqlCommand command = new SqlCommand(queryString, connection);
+
+				// Open the connection in a try/catch block.
+				// Create and execute the DataReader, writing the result
+				// set to the console window.
+				try
+				{
+					connection.Open();
+					SqlDataReader reader = command.ExecuteReader();
+					while (reader.Read())
+					{
+						var emp = new Employee
+						{
+							Id = reader.GetInt32(0),
+							Name = reader.GetString(1),
+							Address = reader.GetString(2),
+							Gender = char.Parse(reader.GetString(3)),
+							Dob = reader.GetDateTime(4),
+						};
+						employees.Add(emp);
+					}
+					reader.Close();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+				return View(employees);
+			}
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Web Development/HumanResources/HumanResources.Web/HumanResources.sql
@@ -0,0 +1,14 @@
+﻿create table Employee
+(
+	Id int not null identity primary key,
+	Name nvarchar(200) not null,
+	Address nvarchar(300),
+	Gender char(1) not null,
+	Dob datetime
+)
+GO
+
+insert into Employee
+values ('Ashesh Shrestha', 'Kathmandu', 'M', '1988/12/12')
+
+select * from Employee;
