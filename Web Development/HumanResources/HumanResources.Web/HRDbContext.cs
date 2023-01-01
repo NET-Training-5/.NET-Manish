8690adc6cda3a518b1c1bacde2dd4e8f8e6754fe
Fauthe <manishlama731@gmail.com>
2023-01-01 06:42:40 +0545

    Learned ORM

---
+++ Web Development/HumanResources/HumanResources.Web/HRDbContext.cs
@@ -0,0 +1,11 @@
+using HumanResources.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class HRDbContext: DbContext
+{
+	public DbSet<Employee> Employees { get; set; }
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=HumanResources; Integrated Security=true");
+	}
+}
